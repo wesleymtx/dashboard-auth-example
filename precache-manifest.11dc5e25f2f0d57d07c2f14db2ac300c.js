5e2ca64843f14249f0c7e429c336a52384432a00
unknown <you@example.com>
2020-09-26 23:30:02 -0300

    Updates

---
+++ precache-manifest.11dc5e25f2f0d57d07c2f14db2ac300c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccac7b7a47817afdf3a27d0329611288",
+    "url": "/dashboard-auth-example/index.html"
+  },
+  {
+    "revision": "b390f975dec566da5b59",
+    "url": "/dashboard-auth-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3e64d0386c59b3f5665",
+    "url": "/dashboard-auth-example/static/js/2.cce7c1c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard-auth-example/static/js/2.cce7c1c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b390f975dec566da5b59",
+    "url": "/dashboard-auth-example/static/js/main.3ffe0803.chunk.js"
+  },
+  {
+    "revision": "1e97b8d3fc8f4e35b4c8",
+    "url": "/dashboard-auth-example/static/js/runtime-main.bbc4f726.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard-auth-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
